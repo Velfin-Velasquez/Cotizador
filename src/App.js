e3bfd98a77af848e3c35d76f6ae5cafdb2678b51
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2020-12-11 10:55:57 -0500

    se agrego Styled Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import { Header } from "./components/Header";
+import styled from "@emotion/styled";
 
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #ffffff;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header
-    titulo="Cotizador"
-    />
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros de autos" />
+      <ContenedorFormulario></ContenedorFormulario>
+    </Contenedor>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import styled from "@emotion/styled";
 
-export const Header = ({titulo}) => {
-    return (
-        <div>
-            <h1>{titulo}</h1>
-        </div>
-    )
-}
+const ContenedorHeader = styled.header`
+  background-color: #26c6da;
+  padding: 10px;
+  font-weight: bold;
+  color: #ffffff;
+`;
+
+const TextoHeader = styled.h1`
+  font-size: 2rem;
+  margin: 0;
+  font-family: "Slabo 27px", serif;
+  text-align: center;
+`;
+
+export const Header = ({ titulo }) => {
+  return (
+    <ContenedorHeader>
+      <TextoHeader>{titulo}</TextoHeader>
+    </ContenedorHeader>
+  );
+};
